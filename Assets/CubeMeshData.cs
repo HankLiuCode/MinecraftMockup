bbcbe0c58a7a2519a96045fe7a22abf1541cc033
unknown <hankpathofprogramming@gmail.com>
2021-02-24 17:28:33 +0800

    Fixed uv not aligned with vertices problem

--- Assets/CubeMeshData.cs
+++ Assets/CubeMeshData.cs
@@ -6,33 +6,24 @@ public static class CubeMeshData
 {
     static Vector3[] cubeVertices = new Vector3[]
     {
-        //new Vector3(1,1,1),
-        //new Vector3(-1,1,1),
-        //new Vector3(-1,-1,1),
-        //new Vector3(1,-1,1),
-        //new Vector3(-1,1,-1),
-        //new Vector3(1,1,-1),
-        //new Vector3(1,-1,-1),
-        //new Vector3(-1,-1,-1)
-
-        new Vector3(1,-1,1),
-        new Vector3(1,1,1),
-        new Vector3(-1,1,1),
-        new Vector3(-1,-1,1),
+        new Vector3( 1,-1, 1),
+        new Vector3( 1, 1, 1),
+        new Vector3(-1, 1, 1),
+        new Vector3(-1,-1, 1),
         new Vector3(-1,-1,-1),
-        new Vector3(-1,1,-1),
-        new Vector3(1,1,-1),
-        new Vector3(1,-1,-1),
+        new Vector3(-1, 1,-1),
+        new Vector3( 1, 1,-1),
+        new Vector3( 1,-1,-1)
     };
 
     static int[][] cubeFaces = new int[][]
     {
         new int[]{0,1,2,3},
-        new int[]{5,0,3,6},
+        new int[]{7,6,1,0},
         new int[]{4,5,6,7},
-        new int[]{1,4,7,2},
-        new int[]{5,4,1,0},
-        new int[]{3,2,7,6}
+        new int[]{3,2,5,4},
+        new int[]{5,2,1,6},
+        new int[]{3,4,7,0}
     };
 
     public static Vector3[] GetFaceVertices(int dir, Vector3 pos, float scale)
