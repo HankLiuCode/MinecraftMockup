a6211fc493607bd33a11cb0704ccb80e526cf458
unknown <hankpathofprogramming@gmail.com>
2021-02-24 17:18:20 +0800

    Initial Commit

---
+++ Assets/CubeMeshData.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CubeMeshData
+{
+    static Vector3[] cubeVertices = new Vector3[]
+    {
+        //new Vector3(1,1,1),
+        //new Vector3(-1,1,1),
+        //new Vector3(-1,-1,1),
+        //new Vector3(1,-1,1),
+        //new Vector3(-1,1,-1),
+        //new Vector3(1,1,-1),
+        //new Vector3(1,-1,-1),
+        //new Vector3(-1,-1,-1)
+
+        new Vector3(1,-1,1),
+        new Vector3(1,1,1),
+        new Vector3(-1,1,1),
+        new Vector3(-1,-1,1),
+        new Vector3(-1,-1,-1),
+        new Vector3(-1,1,-1),
+        new Vector3(1,1,-1),
+        new Vector3(1,-1,-1),
+    };
+
+    static int[][] cubeFaces = new int[][]
+    {
+        new int[]{0,1,2,3},
+        new int[]{5,0,3,6},
+        new int[]{4,5,6,7},
+        new int[]{1,4,7,2},
+        new int[]{5,4,1,0},
+        new int[]{3,2,7,6}
+    };
+
+    public static Vector3[] GetFaceVertices(int dir, Vector3 pos, float scale)
+    {
+        Vector3[] faceVertices = new Vector3[4];
+        for (int i = 0; i < faceVertices.Length; i++)
+        {
+            faceVertices[i] = pos + cubeVertices[cubeFaces[dir][i]] * scale;
+        }
+        return faceVertices;
+    }
+
+    public static Vector3[] GetFaceVertices(Direction dir, Vector3 pos, float scale)
+    {
+        return GetFaceVertices((int)dir, pos, scale);
+    }
+}
+
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West,
+    Up,
+    Down
+}
---
+++ Assets/ProcedureCubeMaker.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProcedureCubeMaker : MonoBehaviour
+{
+    Mesh mesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+    List<Vector2> uvs;
+    float adjScale = 0.5f;
+
+    public float scale = 1f;
+
+
+    private void Awake()
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+        uvs = new List<Vector2>();
+
+        adjScale = scale / 2;
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    public void MakeCube(Vector3 pos)
+    {
+        MakeFace(Direction.North, pos, TilePos.tiles[Tile.GrassSide].GetUVs());
+        MakeFace(Direction.East, pos, TilePos.tiles[Tile.GrassSide].GetUVs());
+        MakeFace(Direction.South, pos, TilePos.tiles[Tile.GrassSide].GetUVs());
+        MakeFace(Direction.West, pos, TilePos.tiles[Tile.GrassSide].GetUVs());
+        MakeFace(Direction.Up, pos, TilePos.tiles[Tile.Grass].GetUVs());
+        MakeFace(Direction.Down, pos, TilePos.tiles[Tile.Dirt].GetUVs());
+    }
+
+    public void MakeFace(Direction dir, Vector3 pos, Vector2[] textureUv)
+    {
+        vertices.AddRange(CubeMeshData.GetFaceVertices(dir, pos, adjScale));
+        int vCount = vertices.Count;
+        triangles.Add(vCount - 4 + 0);
+        triangles.Add(vCount - 4 + 1);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4 + 0);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4 + 3);
+
+        uvs.AddRange(textureUv);
+    }
+
+    public void Clear()
+    {
+        mesh.Clear();
+        vertices.Clear();
+        triangles.Clear();
+        uvs.Clear();
+    }
+
+    public void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.RecalculateNormals();
+    }
+}
